ec61925ce9b0d35559d370bfecdfe435ec66724a
Luca Böhm <lucaboehm709@gmail.com>
2024-02-14 00:02:13 +0100

    add left and right

--- directions.py
+++ directions.py
@@ -1,24 +1,28 @@
 import RPi.GPIO as GPIO
 
-def	forward(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8):
+def	forward(pin1, pin2, pin3, pin4):
 	GPIO.output(pin1, GPIO.HIGH)
 	GPIO.output(pin2, GPIO.LOW)
 	GPIO.output(pin3, GPIO.HIGH)
 	GPIO.output(pin4, GPIO.LOW)
-	GPIO.output(pin5, GPIO.HIGH)
-	GPIO.output(pin6, GPIO.LOW)
-	GPIO.output(pin7, GPIO.HIGH)
-	GPIO.output(pin8, GPIO.LOW)
 
-def	backward(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8):
+def	backward(pin1, pin2, pin3, pin4):
 	GPIO.output(pin1, GPIO.LOW)
 	GPIO.output(pin2, GPIO.HIGH)
 	GPIO.output(pin3, GPIO.LOW)
 	GPIO.output(pin4, GPIO.HIGH)
-	GPIO.output(pin5, GPIO.LOW)
-	GPIO.output(pin6, GPIO.HIGH)
-	GPIO.output(pin7, GPIO.LOW)
-	GPIO.output(pin8, GPIO.HIGH)
+
+def	right(pin1, pin2, pin3, pin4):
+	GPIO.output(pin1, GPIO.HIGH)
+	GPIO.output(pin2, GPIO.LOW)
+	GPIO.output(pin3, GPIO.LOW)
+	GPIO.output(pin4, GPIO.HIGH)
+
+def	left(pin1, pin2, pin3, pin4):
+	GPIO.output(pin1, GPIO.LOW)
+	GPIO.output(pin2, GPIO.HIGH)
+	GPIO.output(pin3, GPIO.HIGH)
+	GPIO.output(pin4, GPIO.LOW)
 
 def	stop(pins):
 	for pin in pins:
--- main.py
+++ main.py
@@ -4,56 +4,52 @@ import directions
 GPIO.setmode(GPIO.BCM)
 
 # Motors
-## Motor 1 = front left
+## Motor 1 = front left,  Motor 3 = back left
 
-OUTFL0 = 17
-OUTFL1 = 27
-OUTFL2 = 22
+OUT1_3_0 = 17
+OUT1_3_1 = 27
+OUT1_3_2 = 22
 
-## Motor 2 = back left
+## Motor 2 = front right, Motor 4 = back right
 
-OUTBL0 = 23
-OUTBL1 = 24
-OUTBL2 = 25
+OUT2_4_0 = 23
+OUT2_4_1 = 24
+OUT2_4_2 = 25
 
-## Motor 3 = front right
-
-OUTFR0 = 5
-OUTFR1 = 6
-OUTFR2 = 13
-
-## Motor 4 = back right
-
-OUTBR0 = 12
-OUTBR1 = 16
-OUTBR2 = 26
-
-PINS = [OUTFL1, OUTFL2, OUTBL1, OUTBL2, OUTFR1, OUTFR2, OUTBR1, OUTBR2]
-PWMS = [OUTFL0, OUTBL0, OUTFR0, OUTBR0]
+PINS = [OUT1_3_1, OUT1_3_2, OUT2_4_1, OUT2_4_2]
+PWMS = [OUT1_3_0, OUT2_4_0]
 for pin in PINS:
 	GPIO.setup(pin, GPIO.OUT)
 
 for pwm in PWMS:
 	GPIO.setup(pwm, GPIO.OUT)
-PWMFL = GPIO.PWM(OUTFL0, 100)
-PWMBL = GPIO.PWM(OUTBL0, 100)
-PWMFR = GPIO.PWM(OUTFR0, 100)
-PWMBR = GPIO.PWM(OUTBR0, 100)
+PWMFL = GPIO.PWM(OUT1_3_0, 100)
+PWMBL = GPIO.PWM(OUT2_4_0, 100)
 
 PWMFL.start(0)
 PWMBL.start(0)
-PWMFR.start(0)
-PWMBR.start(0)
 
 print('Forward')
-directions.forward(OUTFL1, OUTFL2, OUTBL1, OUTBL2, OUTFR1, OUTFR2, OUTBR1, OUTBR2)
-time.sleep(5)
+directions.forward(OUT1_3_1, OUT1_3_2, OUT2_4_1, OUT2_4_2)
+time.sleep(3)
 print('Stop')
 directions.stop(PINS)
-time.sleep(5)
+time.sleep(3)
 print('Backward')
-directions.backward(OUTFL1, OUTFL2, OUTBL1, OUTBL2, OUTFR1, OUTFR2, OUTBR1, OUTBR2)
-time.sleep(5)
+directions.backward(OUT1_3_1, OUT1_3_2, OUT2_4_1, OUT2_4_2)
+time.sleep(3)
+print('Stop')
+directions.stop(PINS)
+time.sleep(3)
+print('Right')
+directions.right(OUT1_3_1, OUT1_3_2, OUT2_4_1, OUT2_4_2)
+time.sleep(3)
+print('Stop')
+directions.stop(PINS)
+time.sleep(3)
+print('Left')
+directions.left(OUT1_3_1, OUT1_3_2, OUT2_4_1, OUT2_4_2)
+time.sleep(3)
 print('End')
 directions.stop(PINS)
 GPIO.cleanup()
