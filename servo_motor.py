b7b27ff570af93d60c9da0e17943ea188e8838a7
Lucabhm <lucaboehm709@gmail.com>
2024-04-25 21:06:57 +0100

    ka

--- servo_motor.py
+++ servo_motor.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
+servoPIN = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
@@ -9,6 +9,7 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
 p.start(2.5) # Initialisierung
 try:
   while True:
+    print("here")
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
