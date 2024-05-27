17ec6324fa63318e74b4a4965bfee550ed9aedf4
Lucabhm <lbohm@student.42heilbronn.de>
2024-05-27 20:45:04 +0200

    test motors

--- test.py
+++ test.py
@@ -1,13 +1,30 @@
 import RPi.GPIO as GPIO
+import directions as d
+import time
 
+sensor = 6
+sensor1 = 13
+sensor2 = 19
+sensor3 = 26
 
-sensor = 12
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensor,GPIO.IN)
-try: 
-   while True:
-      if GPIO.input(sensor):
-          print ("Object Detected")
+GPIO.setmode(GPIO.BCM)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+GPIO.setup(sensor,GPIO.OUT)
+GPIO.setup(sensor1,GPIO.OUT)
+GPIO.setup(sensor2,GPIO.OUT)
+GPIO.setup(sensor3,GPIO.OUT)
+
+d.forward(sensor, sensor1, sensor2, sensor3)
+time.sleep(2)
+d.stop(sensor, sensor1, sensor2, sensor3)
+d.backward(sensor, sensor1, sensor2, sensor3)
+time.sleep(2)
+d.stop(sensor, sensor1, sensor2, sensor3)
+d.right(sensor, sensor1, sensor2, sensor3)
+time.sleep(2)
+d.stop(sensor, sensor1, sensor2, sensor3)
+d.left(sensor, sensor1, sensor2, sensor3)
+time.sleep(2)
+d.stop(sensor, sensor1, sensor2, sensor3)
+
+GPIO.cleanup()
