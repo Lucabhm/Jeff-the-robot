58c99238948d8dcf0b6c7dedb48f87effeb5b446
Lucabhm <lbohm@student.42heilbronn.de>
2024-05-31 22:42:14 +0200

    test servo

--- test.py
+++ test.py
@@ -1,30 +1,15 @@
 import RPi.GPIO as GPIO
-import directions as d
 import time
 
-sensor = 6
-sensor1 = 13
-sensor2 = 19
-sensor3 = 26
-
+servoPIN = 17
+i = 0
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(sensor,GPIO.OUT)
-GPIO.setup(sensor1,GPIO.OUT)
-GPIO.setup(sensor2,GPIO.OUT)
-GPIO.setup(sensor3,GPIO.OUT)
-
-d.forward(sensor, sensor1, sensor2, sensor3)
-time.sleep(2)
-d.stop(sensor, sensor1, sensor2, sensor3)
-d.backward(sensor, sensor1, sensor2, sensor3)
-time.sleep(2)
-d.stop(sensor, sensor1, sensor2, sensor3)
-d.right(sensor, sensor1, sensor2, sensor3)
-time.sleep(2)
-d.stop(sensor, sensor1, sensor2, sensor3)
-d.left(sensor, sensor1, sensor2, sensor3)
-time.sleep(2)
-d.stop(sensor, sensor1, sensor2, sensor3)
+GPIO.setup(servoPIN, GPIO.OUT)
+p = GPIO.PWM(servoPIN, 50)
+p.start(i)
+for i in range(0, 12.5, 0.25):
+	p.ChangeDutyCycle(i)
+for i in range(12.5, 0, -0.25):
+	p.ChangeDutyCycle(i)
 
 GPIO.cleanup()
