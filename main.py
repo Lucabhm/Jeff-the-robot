7de0adeb75a2eb6a1d48c5ea4628b150d5ab3a49
Lucabhm <lbohm@student.42heilbronn.de>
2024-06-05 21:51:06 +0200

    try new child process

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ else:
 	distanz_val = 0
 	i = 6.5
 	queue = multiprocessing.Queue()
-	p2 = multiprocessing.Process(target=us.distanz, args=(queue, OUT5_0, OUT5_1))
 #	p1 = Process(target=sm.set_to_zero)
 #	pl = Process(target=directions.left, args=(OUT1_1, OUT1_2, OUT2_1, OUT2_2))
 #	pr = Process(target=directions.right, args=(OUT1_1, OUT1_2, OUT2_1, OUT2_2))
@@ -62,11 +61,10 @@ else:
 				print("here")
 				while not GPIO.input(OUT4_0) and not GPIO.input(OUT4_1):
 					print("inside loop")
-					if not p2.is_alive():
-						p2.start()
+					p2 = multiprocessing.Process(target=us.distanz, args=(queue, OUT5_0, OUT5_1))
+					p2.start()
 					distanz_val = queue.get()
-					if p2.is_alive():
-						p2.join()
+					p2.terminate()
 					print("distanz = %.1f" % distanz_val)
 					if (distanz_val < 10):
 						print("stop")
